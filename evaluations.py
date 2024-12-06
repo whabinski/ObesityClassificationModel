a836ab839a9162ce36a371a810a0bca69deb591a
Alex Eckardt <realalexeck@gmail.com>
2024-12-06 14:13:38 -0500

    Evaluation File

---
+++ evaluations.py
@@ -0,0 +1,7 @@
+
+# ... accuracy, precision, recall and f1-score metrics which will be
+# used for model evaluation as it ensures a comprehensive and reliable assessment for
+# classification models.
+def evaluations(models, Xtest, Yest):
+    for model in models:
+        pass
