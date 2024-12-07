7dcd33853a5ccb3ef1f2089d21be641ceb6a9602
ericzz12 <80360373+ericzz12@users.noreply.github.com>
2024-12-07 18:28:58 -0500

    modified logistic regression

--- Models/logistic_regression.py
+++ Models/logistic_regression.py
@@ -4,27 +4,31 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 
-class LogisticRegression(Model, nn.Module): 
+class LRModel(nn.Module):
     def __init__(self, n_inputs=31, n_classes=7):
-        super(LogisticRegression, self).__init__()
-        self.linear = nn.Linear(n_inputs, n_classes) 
+        super(LRModel, self).__init__()
+        self.linear = nn.Linear(n_inputs, n_classes)
 
     def forward(self, x):
         return self.linear(x).squeeze(-1) # squeeze to change shape from (n, 1) to (n,)
 
-    def train_model(self, X, Y, learning_rate=0.1, epochs=1000):
+class LogisticRegression(Model): 
+    def __init__(self):
+        self.model = LRModel()
+
+    def train(self, X, Y, learning_rate=0.1, epochs=1000):
         # criterions: 
         # nn.CrossEntropyLoss() - evaluate as classification (ordinal doesnt matter)
         # nn.MSELoss() - evaluate as regression (classes dont matter much)
         criterion = nn.CrossEntropyLoss()
-        optimizer = optim.SGD(self.parameters(), lr=learning_rate)
+        optimizer = optim.SGD(self.model.parameters(), lr=learning_rate)
         X_ = torch.from_numpy(X).float()
         Y_ = torch.from_numpy(Y).long()
 
         for epoch in range(epochs):
-            self.train()
+            self.model.train()
             optimizer.zero_grad()
-            outputs = self.forward(X_).squeeze(-1)
+            outputs = self.model.forward(X_).squeeze(-1)
             loss = criterion(outputs, Y_)
             loss.backward()
             optimizer.step()
@@ -33,9 +37,9 @@ class LogisticRegression(Model, nn.Module):
 
     def predict(self, X):
         X_ = torch.from_numpy(X).float()
-        self.eval()
+        self.model.eval()
         with torch.no_grad():
-            outputs = self.forward(X_)
+            outputs = self.model.forward(X_)
             _, predicted = torch.max(outputs.data, 1)
             return predicted 
 
