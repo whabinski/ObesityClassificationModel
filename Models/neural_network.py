c6441dc93e25025a1fbe1944c088f324de72d279
Alex Eckardt <realalexeck@gmail.com>
2024-12-06 23:14:59 -0500

    Basic NN

--- Models/neural_network.py
+++ Models/neural_network.py
@@ -1,5 +1,25 @@
 from Models.model import Model
+import torch
+import torch.nn as nn
+import torch.optim as optim
+
+class NNClass(nn.Module):
+    def __init__(self):
+        super(NNClass, self).__init__()
+        self.relu = nn.ReLU()
+        self.f1 = nn.Linear(1, 1)
+
+    def forward(self, x):
+        return self.relu(self.f1(x));
 
 class NeuralNetwork(Model):
-    def __init__(self):
+    def __init__(self, learningRate):
+        self.model = NNClass()
+        self.optimizer = optim.SGD(self.model.parameters(), lr=learningRate)
+        self.criterion = nn.CrossEntropyLoss()
+
+    def train(self, features, labels):
+        pass
+
+    def predict(self, features):
         pass
